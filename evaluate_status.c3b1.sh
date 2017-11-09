138578dd614f74a1f536022f52f383cad7aca0e9
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2017-11-09 16:57:09 -0600

    Funtional set of scripts for downloading CPTAC3 batch 1

---
+++ evaluate_status.c3b1.sh
@@ -0,0 +1,25 @@
+# This is a wrapper around importGDC/evaluate_status.sh with CPTAC3.b1-specific setup added for convenience
+# All arguments passed to here will be passed to evaluate_status.sh
+
+# Usage from evaluate_status.sh
+
+# Evaluate status of all samples in batch file 
+# This is specific to MGI (dependent on LSF-specific output to evaluate status)
+# Usage: evaluate_status.sh [options] batch.dat
+#
+# options
+# -f status: output only lines matching status, e.g., -f import:completed
+# -u: include only UUID in output
+# -D: include data file path in output
+
+# CPTAC3.b1 MGI data download location
+DATA_DIR="/gscmnt/gc2521/dinglab/mwyczalk/CPTAC3-download/"
+mkdir -p $DATA_DIR
+
+export IMPORTGDC_HOME="/gscuser/mwyczalk/src/importGDC"
+
+
+# Example 
+# evaluate_status.c3b1.sh -u -f import:ready dat/WXS.batch.dat 
+
+bash $IMPORTGDC_HOME/batch.import/evaluate_status.sh -O $DATA_DIR "$@"
---
+++ start_import.c3b1.sh
@@ -0,0 +1,34 @@
+# Start import of given UUIDs.  This is run on host computer
+
+# This is a wrapper around importGDC/start_step.sh with CPTAC3.b1-specific setup added for convenience
+# All arguments passed to here will be passed to start_step.sh
+
+# Using LSF_Group /mwyczalk/gdc-download (TODO: allow this to be defined in environment variable)
+
+# Usage: start_import.c3b1.sh [options] UUID [UUID2 ...]
+# Start import on host computer.  
+# options:
+# -d: dry run
+#
+# If UUID is - then read UUID from STDIN
+
+# Data download location
+DATA_DIR="/gscmnt/gc2521/dinglab/mwyczalk/CPTAC3-download/"
+mkdir -p $DATA_DIR
+
+LSF_GROUP="-g /mwyczalk/gdc-download"
+
+# Should define IMPORTGDC_HOME
+export IMPORTGDC_HOME="/gscuser/mwyczalk/src/importGDC"
+
+SR="config/SR.CPTAC3.b1.dat"
+
+# Copy token defined in host dir to container's /data directory
+# Note there may be some security considerations associated with this
+TOKEN_HOST="token/gdc-user-token.2017-11-04T01-21-42.215Z.txt"
+TOKEN_CONTAINER="/data/token/gdc-user-token.txt"
+mkdir -p $DATA_DIR/token
+>&2 echo Copying $TOKEN_HOST to $DATA_DIR/token/gdc-user-token.txt
+cp $TOKEN_HOST $DATA_DIR/token/gdc-user-token.txt
+
+bash $IMPORTGDC_HOME/batch.import/start_step.sh -O $DATA_DIR -S $SR $LSF_GROUP -s import "$@"
