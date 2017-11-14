39a124cfaf1bbdcda748daf24367fc133e733326
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2017-11-14 11:07:31 -0600

    Allowing download on MGI

--- start_import.c3b1.sh
+++ start_import.c3b1.sh
@@ -28,10 +28,11 @@ fi
 
 # Copy token defined in host dir to container's /data directory
 # Note there may be some security considerations associated with this
+TOKEN_HOST="token/gdc-user-token.2017-11-14T15_55_43.552Z.txt"
 TOKEN_CONTAINER="/data/token/gdc-user-token.txt"
 mkdir -p $DATA_DIR/token
 >&2 echo Copying $GDCTOKEN to $DATA_DIR/token/gdc-user-token.txt
 cp $GDCTOKEN $DATA_DIR/token/gdc-user-token.txt
 
 
-bash $IMPORTGDC_HOME/batch.import/start_step.sh -O $DATA_DIR -S $SR $LSF_GROUP_ARG -s import "$@"
+bash $IMPORTGDC_HOME/batch.import/start_step.sh -M -O $DATA_DIR -S $SR $LSF_GROUP_ARG -s import "$@"
