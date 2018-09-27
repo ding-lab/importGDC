23a3512ec51dc2453b2adba40a4f8051b4e9a525
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-09-27 14:42:43 -0500

    Updating extracting of cases from master SR file

--- 1_get_SR.sh
+++ 1_get_SR.sh
@@ -1,19 +1,44 @@
 # Here, we use the C325 SR file generated on denali here:
 # We select a subset of cases from the file /Users/mwyczalk/Projects/CPTAC3/CPTAC3.Cases/dat/CPTAC3.C325.cases.dat 
 # 
+
+# Usage: get_size_by_type TYPE
+# where TYPE is WGS, WXS, RNA-Seq
+function get_size_by_type {
+        grep -v "^#" $DAT | awk -v t=$1 '{if ($4 == t) print}' | cut -f 8 | awk '{s+=$1} END {print s / 1024 / 1024 / 1024 / 1024}'
+        #SIZE=$(grep -v "^#" $DAT | cut -f 8 | awk '{s+=$1} END {print s / 1024 / 1024 / 1024 / 1024}')
+}
+
+function summarize {
+DAT=$1
+WGS_SIZE=$(get_size_by_type WGS)
+WXS_SIZE=$(get_size_by_type WXS)
+RNA_SIZE=$(get_size_by_type RNA-Seq)
+
+echo $DAT
+echo "Total required disk space WGS: $WGS_SIZE Tb"
+echo "                          WXS: $WXS_SIZE Tb"
+echo "                          RNA-Seq: $RNA_SIZE Tb"
+}
+
+
+
+
 CASES="../CommonFiles/CPTAC3.C325.cases.dat"
 SR="../CommonFiles/CPTAC3.C325.SR.dat"
 
-SEARCH_BATCH="b5"  # This has to match column in cases file
-OUT_BATCH="CCRC.b5"  # This should match what's in gdc-import.config.sh
-DIS="CCRC"
+BATCH="b4"  # This has to match column in cases file
+DIS="LUAD"
 
 mkdir -p dat
 TMP="dat/cases.tmp"
-SR_NEW="dat/CPTAC3.${OUT_BATCH}.SR.dat"
+SR_NEW="dat/CPTAC3.${DIS}.${BATCH}.SR.dat"
 
-awk -v batch=$SEARCH_BATCH -v dis=$DIS 'BEGIN{FS="\t"; OFS="\t"}{if (($3 == batch) && ($2 == dis)) print $1}' $CASES  > $TMP
+awk -v batch=$BATCH -v dis=$DIS 'BEGIN{FS="\t"; OFS="\t"}{if (($3 == batch) && ($2 == dis)) print $1}' $CASES  > $TMP
 
-grep -f $TMP $SR > $SR_NEW
+head -n1 $SR > $SR_NEW
+grep -f $TMP $SR >> $SR_NEW
 
 echo Written to $SR_NEW
+
+summarize $SR_NEW
