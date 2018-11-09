1f3e50e69673c0686c86d7d66faf597ea26291a5
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2018-11-09 14:05:53 -0600

    Updating importGDC and configuration file format

--- gdc-import.config.sh
+++ gdc-import.config.sh
@@ -1,13 +1,27 @@
 # Download batch name.  
-BATCH="UCEC.WGS.hg38.all"
+BATCH="hg38.WXS.sc13"
 
 # Data download root directory.  Individual BAMS/FASTQs will be in,
 #   $STAGE_ROOT/GDC_import/data/<UUID>/<FILENAME>
-STAGE_ROOT="/diskmnt/Projects/cptac_downloads_5"
 
 # Download token from GDC, good for 30 days.  Generating a new one causes old ones to break
 GDC_TOKEN="../token/gdc-user-token.2018-10-27T22_53_11.029Z.txt"
 
+# Master SR file containing all samples.  We will download a subset of these
+# Master BamMap file which hold most current list of BamMaps on system.  This file will not be modified by any scripts 
+
+## KATMAI
+#SR_MASTER="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/CPTAC3.SR.dat"
+#BAMMAP_MASTER="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/katmai.BamMap.dat"
+#STAGE_ROOT="/diskmnt/Projects/cptac_downloads_5"
+#MGI=1
+
+# MGI
+SR_MASTER="/gscuser/mwyczalk/projects/CPTAC3/CPTAC3.catalog/CPTAC3.SR.dat"
+BAMMAP_MASTER="/gscuser/mwyczalk/projects/CPTAC3/CPTAC3.catalog/MGI.BamMap.dat"
+STAGE_ROOT="/gscmnt/gc2741/ding/CPTAC3-data"
+# Define this =1 if in MGI environment, =0 otherwise
+MGI=1
 
 # This is where download-related metadata lives (config files, logs, etc)
 IMPORT_CONFIGD_H="$STAGE_ROOT/GDC_import/import.config/$BATCH"
@@ -18,20 +32,10 @@ mkdir -p $IMPORT_CONFIGD_H
 # this is used in importGDC scripts
 export IMPORTGDC_HOME="./importGDC"  
 
-# Master SR file containing all samples.  We will download a subset of these
-#SR_MASTER="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/CPTAC3.SR.dat"
-SR_MASTER="/gscuser/mwyczalk/projects/CPTAC3/CPTAC3.catalog/CPTAC3.SR.dat"
-
 # This is the SR file which will drive processing here: all samples in this file will be downloaded
 # This file is generated in step 2 as a subset of SR_MASTER
 SR_H="dat/$BATCH.SR.dat"
 
-# Master BamMap file which hold most current list of BamMaps on system.  This file will not be 
-# modified by any scripts 
-BAMMAP_MASTER="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/katmai.BamMap.dat"
-
 # BAMMAP is created as the final step of import process.
 BAMMAP="dat/${BATCH}.BamMap.dat"
 
-# Define this =1 if in MGI environment, =0 otherwise
-MGI=1
