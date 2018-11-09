cc07610f4fe830aebe40f86705cc99f6edff3c58
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2018-11-09 12:03:02 -0600

    Bugfix

--- 9_make_BamMap.sh
+++ 9_make_BamMap.sh
@@ -12,12 +12,14 @@ REF="-r $REFERENCE"
 bash importGDC/make_bam_map.sh -H > $BAMMAP
 bash importGDC/make_bam_map.sh -O $STAGE_ROOT -S $SR_H $REF - | sort >> $BAMMAP
 
-rc=$?
-if [[ $rc != 0 ]]; then
-    >&2 echo Written to $BAMMAP
-    >&2 echo Errors / warnings $rc: $!.  Exiting.
-    exit $rc;
-fi
+# Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+rcs=${PIPESTATUS[*]};
+for rc in ${rcs}; do
+	if [[ $rc != 0 ]]; then
+		>&2 echo Fatal error.  Exiting.
+		exit $rc;
+	fi;
+done
 
 # If no errors, write merged BamMap, which is BAMMAP and BAMMAP_MASTER concatenated and sorted
 # The output filename will be $BAMMAP.merged
