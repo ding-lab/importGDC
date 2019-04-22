dc37cfb354ed9367cc56df00e02ab2b22738ea9b
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-04-22 16:52:40 -0500

    Minor bug fixes

--- 4_make_BamMap.sh
+++ 4_make_BamMap.sh
@@ -8,14 +8,14 @@
 source gdc-import.config.sh
 
 bash importGDC/make_bam_map.sh -H > $BAMMAP
-bash importGDC/make_bam_map.sh -O $STAGE_ROOT -S $AR_H -s $SYSTEM - | sort >> $BAMMAP
+bash importGDC/make_bam_map.sh -O $STAGE_ROOT -S $AR_H -s $SYSTEM $@ - | sort >> $BAMMAP
 
 # Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
 rcs=${PIPESTATUS[*]};
 for rc in ${rcs}; do
 	if [[ $rc != 0 ]]; then
 		>&2 echo Errors / warnings processing BamMap 
-        >&2 echo Written successfully to $BAMMAP
+        >&2 echo Partly written to $BAMMAP
         >&2 echo Stopping.  Will not merge with Master BamMap.
 		exit $rc;
 	fi;
