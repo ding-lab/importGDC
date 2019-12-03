8b3dbdf22ba4e5a009e35114d29048ffcbd89d40
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-12-03 14:09:28 -0600

    Bug fix to work with Methylation Array

--- 2_get_Catalog.sh
+++ 2_get_Catalog.sh
@@ -1,22 +1,40 @@
 
 source gdc-import.config.sh
 
+# Catalog format
+#     1    # sample_name
+#     2  case
+#     3  disease
+#     4  experimental_strategy
+#     5  short_sample_type
+#     6  aliquot
+#     7  filename
+#     8  filesize
+#     9  data_format
+#    10  result_type
+#    11  UUID
+#    12  MD5
+#    13  reference
+#    14  sample_type
+
 # Usage: get_size_by_type TYPE
 # where TYPE is WGS, WXS, RNA-Seq
 function get_size_by_type {
-        grep -v "^#" $DAT | awk -v t=$1 '{if ($4 == t) print}' | cut -f 8 | awk '{s+=$1} END {print s / 1024 / 1024 / 1024 / 1024}'
+        grep -v "^#" $DAT | awk -v t="$1" 'BEGIN{FS="\t"}{if ($4 == t) print}' | cut -f 8 | awk '{s+=$1} END {print s / 1024 / 1024 / 1024 / 1024}'
         #SIZE=$(grep -v "^#" $DAT | cut -f 8 | awk '{s+=$1} END {print s / 1024 / 1024 / 1024 / 1024}')
 }
 
 # Usage: get_count_by_type TYPE
 # where TYPE is WGS, WXS, RNA-Seq
 function get_count_by_type {
-        grep -v "^#" $DAT | awk -v t=$1 '{if ($4 == t) print}' | wc -l 
+        grep -v "^#" $DAT | awk -v t="$1" 'BEGIN{FS="\t"}{if ($4 == t) print}' | wc -l 
         #SIZE=$(grep -v "^#" $DAT | cut -f 8 | awk '{s+=$1} END {print s / 1024 / 1024 / 1024 / 1024}')
 }
 
 function summarize {
 DAT=$1
+
+>&2 echo Summarizing $DAT
 WGS_SIZE=$(get_size_by_type WGS)
 WGS_COUNT=$(get_count_by_type WGS)
 
@@ -29,21 +47,26 @@ RNA_COUNT=$(get_count_by_type RNA-Seq)
 MIRNA_SIZE=$(get_size_by_type miRNA-Seq)
 MIRNA_COUNT=$(get_count_by_type miRNA-Seq)
 
-TOT_SIZE=$(echo "$WGS_SIZE + $WXS_SIZE + $RNA_SIZE + $MIRNA_SIZE" | bc)
-TOT_COUNT=$(echo "$WGS_COUNT + $WXS_COUNT + $RNA_COUNT + $MIRNA_COUNT" | bc)
+METH_SIZE=$(get_size_by_type "Methylation Array")
+METH_COUNT=$(get_count_by_type "Methylation Array")
 
-echo $DAT
+TOT_SIZE=$(echo "$WGS_SIZE + $WXS_SIZE + $RNA_SIZE + $MIRNA_SIZE + $METH_SIZE" | bc)
+TOT_COUNT=$(echo "$WGS_COUNT + $WXS_COUNT + $RNA_COUNT + $MIRNA_COUNT + $METH_COUNT" | bc)
+
 echo "Total required disk space WGS: $WGS_SIZE Tb in $WGS_COUNT files"
 echo "                          WXS: $WXS_SIZE Tb in $WXS_COUNT files"
 echo "                      RNA-Seq: $RNA_SIZE Tb in $RNA_COUNT files"
 echo "                    miRNA-Seq: $MIRNA_SIZE Tb in $MIRNA_COUNT files"
+echo "            Methylation Array: $METH_SIZE Tb in $METH_COUNT files"
 echo "                        TOTAL: $TOT_SIZE Tb in $TOT_COUNT files"
 }
 
 mkdir -p dat
-UUID="dat/UUID-download.dat"
+UUID="dat/UUID-all.dat"
+
+>&2 echo Catalog: $CATALOG_MASTER
 
 head -n1 $CATALOG_MASTER > $CATALOG_H
-grep -f $UUID $CATALOG_MASTER >> $CATALOG_H
+fgrep -f $UUID $CATALOG_MASTER >> $CATALOG_H
 
 summarize $CATALOG_H
