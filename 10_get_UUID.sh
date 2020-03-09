d4656d41b50214a7c78cf36e3a92f592824342da
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-03-09 17:53:23 -0500

    Adding template for UUID discovery

--- 10_get_UUID.sh
+++ 10_get_UUID.sh
@@ -1,10 +1,30 @@
 # This is very project-specific
-# Here, obtaining all PDA Genomic RNA-Seq data and writing UUIDs to download to $OUT
+# Here, obtain all WGS data for the following diseases which does not exist on katmai
+# * HNSCC
+# * PDA
+# * LUAD
+# * CCRCC
+# * UCEC
 
 source gdc-import.config.sh
 
-OUT=$UUID
+OUT1="dat/UUID-Catalog.dat"
+OUT2="dat/UUID-katmai.dat"
 mkdir -p dat
 
-grep PDA $CATALOG_MASTER | grep genomic | cut -f 11 | sort > $OUT
-echo Written to $OUT
+OUT=$UUID
+
+#PRIORITY="HNSCC\|PDA\|LUAD\|CCRCC\|UCEC"
+PRIORITY="HNSCC\|PDA"
+
+grep "$PRIORITY" $CATALOG_MASTER | grep WGS | grep hg38 | cut -f 11 | sort > $OUT1
+echo Written all UUID to $OUT1
+
+# now get all WGS hg38 UUIDs which are available on katmai
+echo $BAMMAP_MASTER
+grep "$PRIORITY" $BAMMAP_MASTER | grep WGS | grep hg38 | cut -f 10 | sort > $OUT2
+echo Written katmai UUID to $OUT2
+
+# Get all UUIDs which exist in catalog but not katmai
+comm -23 $OUT1 $OUT2 > $OUT
+echo Written download UUID to $OUT
