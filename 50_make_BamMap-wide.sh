07a0b38f46b4fc14abe2e4d645a773fb32082c49
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2023-02-14 13:18:27 -0600

    Minor bugfix to track project names better

--- 50_make_BamMap-wide.sh
+++ 50_make_BamMap-wide.sh
@@ -1,12 +1,13 @@
 # Creating Bammap-wide file.  If run on compute1, need to be running inside docker
+source gdc-import.config.sh
 
 PYTHON=/usr/bin/python3
 
 CATALOGD="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog"
-CATALOG="$CATALOGD/Catalog3/MILD.Catalog3.tsv"
+CATALOG="$CATALOGD/Catalog3/${PROJECT}.Catalog3.tsv"
 #BM3="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/import/02.MILD_WXS_311/dat/batch.BamMap.dat.merged"
 BM3="dat/batch.BamMap.dat.merged"
-OUT="dat/MILD.BamMap-wide.tsv"
+OUT="dat/${PROJECT}.BamMap-wide.tsv"
 
 CMD="$PYTHON src/make_BamMap-wide.py $@ -o $OUT $BM3 $CATALOG"
 >&2 echo Running: $CMD
