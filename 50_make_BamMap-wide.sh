14eba58511ab253e2450090841a0b35f2239ba4c
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2023-10-23 14:35:56 -0500

    Notes about bammap-wide paths

--- 50_make_BamMap-wide.sh
+++ 50_make_BamMap-wide.sh
@@ -10,10 +10,16 @@ PYTHON="./python3_docker"
 #CATALOG="$CATALOGD/Catalog3/${PROJECT}.Catalog3.tsv"
 #FileNotFoundError: [Errno 2] File b'/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/CTSP_DLBCL.Catalog3.tsv' does not exist: b'/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/CTSP_DLBCL.Catalog3.tsv'
 
-#CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/DLBCL.GDC_REST.20230409-AWG.tsv"
+
+# top one does not work
 #CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/HCMI.Catalog-REST.tsv"
+CATALOG="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/HCMI.Catalog-REST.tsv"
+
+
+
+#CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/DLBCL.GDC_REST.20230409-AWG.tsv"
 #CATALOG="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/MILD.Catalog-REST.tsv"
-CATALOG="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/CTSP_DLBCL.Catalog-REST.tsv"
+#CATALOG="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/CTSP_DLBCL.Catalog-REST.tsv"
 #CATALOG="/storage1/fs1/dinglab/Active/Projects/CPTAC3/Common/CPTAC3.catalog/Catalog3/CPTAC3.Catalog3.tsv"
 
 #BM3="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/GDAN/import/02.MILD_WXS_311/dat/batch.BamMap.dat.merged"
