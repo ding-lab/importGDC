c6ace2a5d27d4c00098b11a7ba1ede53ce6b3ddc
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2026-04-01 15:02:03 -0500

    Minor update to gdc-import

--- gdc-import.config.sh
+++ gdc-import.config.sh
@@ -1,10 +1,10 @@
 # Download batch name.  
 
-PROJECT="TCGA"
+PROJECT="CPTAC3"
 CAT_TYPE="REST" # Catalog3 or REST
 
 # Download token from GDC, good for 30 days.  Generating a new one causes old ones to break
-GDC_TOKEN="../token/gdc-user-token.2025-09-04T16_53_36.318Z.txt"
+GDC_TOKEN="../token/gdc-user-token.2026-03-03T20_01_37.928Z.txt"
 
 # Format: /USER/gdc-download
 # Create with `bgadd -L 5 /USER/gdc-download`
@@ -26,23 +26,34 @@ LSF=1
 DL_ARGS="-M -q dinglab" 
 LSF_ARGS="-g $LSF_GROUP -G compute-dinglab"
 
-# This differs for GDAN and CPTAC3 systems
+# Catalog location differs for GDAN and CPTAC3 systems
 if [ $PROJECT == "CPTAC3" ]; then
 # for CPTAC3
     CATALOGD="/storage1/fs1/dinglab/Active/Projects/CPTAC3/Common/CPTAC3.catalog"
-    BAMMAP_MASTER="$CATALOGD/Catalog3/WUSTL-BamMap/${FILE_SYSTEM}.BamMap3.tsv"
+#    BAMMAP_MASTER="$CATALOGD/Catalog3/WUSTL-BamMap/${FILE_SYSTEM}.BamMap3.tsv"
 else
 # for GDAN
     CATALOGD="/rdcw/fs2/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog"
-    BAMMAP_MASTER="$CATALOGD/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap3.tsv"
-    BAMMAP_WIDE="$CATALOGD/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap-wide.tsv"
+#    BAMMAP_MASTER="$CATALOGD/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap3.tsv"
 fi
 
-# note that CPTAC3 can have catalog3 or REST catalogs
-if [ $CAT_TYPE == "REST" ]; then
-    CATALOG_MASTER="$CATALOGD/Catalog3/${PROJECT}.Catalog-REST.tsv"  
-else
-    CATALOG_MASTER="$CATALOGD/Catalog3/${PROJECT}.Catalog3.tsv"
+# note that CPTAC3 used to have catalog3 or REST catalogs
+# now, after CPTAC3 / GDAN merge only REST catalogs are supported
+#if [ $CAT_TYPE == "REST" ]; then
+CATALOG_MASTER="$CATALOGD/Catalog3/${PROJECT}.Catalog-REST.tsv"  
+#else
+#    CATALOG_MASTER="$CATALOGD/Catalog3/${PROJECT}.Catalog3.tsv"
+#fi
+if [ ! -e $CATALOG_MASTER ]; then
+    >&2 echo ERROR: Catalog does not exist: $CATALOG_MASTER
+    exit 1
+fi
+
+BAMMAPD="/rdcw/fs2/home1/Active/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog"
+BAMMAP_MASTER="$CATALOGD/Catalog3/WUSTL-BamMap/storage1-GDC-BamMap4.tsv"
+if [ ! -e $BAMMAP_MASTER ]; then
+    >&2 echo ERROR: BamMap does not exist: $BAMMAP_MASTER
+    exit 1
 fi
 
 # This file is generated in step 2 as a subset of CATALOG_MASTER
